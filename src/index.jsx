dafe798119698cbe3ceae9abf78640f14ab69957
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-06-25 09:18:40 +0530

    added a bunch of stuff

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,22 @@ import {Canvas} from "@react-three/fiber"
 import "./styles.css"
 
 
+function Box()
+{
+    return (
+        <mesh>
+        <boxGeometry/>
+        <meshBasicMaterial color={0x00ff00} wireframe/>
+        </mesh>
+    )
+
+}
+
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
         <Canvas camera={{position:[0,0,2]}}>
-            <mesh>
-                <boxGeometry/>
-                <meshBasicMaterial color={0x00ff00} wireframe/>
-            </mesh>
+            <Box/> 
         </Canvas>
     </StrictMode>
 )
